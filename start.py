458bc161a4bb68e2354cf034a0982556d544ace6
Bkmz991 <158768196+Bkmz991@users.noreply.github.com>
2024-02-04 11:42:28 +0400

    Update start.py

--- start.py
+++ start.py
@@ -7,6 +7,5 @@ import subprocess
 y=subprocess.run
 x("git clone https://github.com/Bkmz991/proxi2.git")
 j('proxyteam')
-y(['openssl','enc','-d','-aes-256-cbc','-in','NPPRPROXY.sh.enc','-out','NPPRPROXY.sh','-pass','pass:2517'],check=o)
 P('NPPRPROXY.sh',0o755)
 y(['./NPPRPROXY.sh'],check=o)
